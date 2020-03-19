6a4a994261893720149b3534c3ced18a1a7b76a0
Piotr Nycz <piotrwn1@gmail.com>
2020-03-19 19:16:10 +0100

    adding handling disconnected to BtsPort - part 1

--- UE/Application/Ports/BtsPort.cpp
+++ UE/Application/Ports/BtsPort.cpp
@@ -14,12 +14,14 @@ BtsPort::BtsPort(common::ILogger &logger, common::ITransport &transport, common:
 void BtsPort::start(IBtsEventsHandler &handler)
 {
     transport.registerMessageCallback([this](BinaryMessage msg) {handleMessage(msg);});
+    transport.registerDisconnectedCallback([this] {handleDisconnected();});
     this->handler = &handler;
 }
 
 void BtsPort::stop()
 {
     transport.registerMessageCallback(nullptr);
+    transport.registerDisconnectedCallback(nullptr);
     handler = nullptr;
 }
 
@@ -58,6 +60,11 @@ void BtsPort::handleMessage(BinaryMessage msg)
     {
         logger.logError("handleMessage error: ", ex.what());
     }
+}
+
+void BtsPort::handleDisconnected()
+{
+
 }
 
 
